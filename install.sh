0108d863466a980676c638aeefc3d1d97fe3cb0f
Devin Marks <dmarks2706@floridapoly.edu>
2019-03-26 01:44:17 -0400

    Added functionality to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+
 
 sudo add-apt-repository ppa:deadsnakes/ppa;
 sudo apt update;
@@ -8,9 +8,9 @@ sudo apt update;
 sudo snap install discord;
 sudo snap install telegram-desktop;
 sudo snap install spotify;
-sudo snap install vscode;
+sudo snap install vscode --classic;
 
-sudo apt isntall -y python3.6;
+sudo apt install -y python3.6;
 sudo apt install -y python3-pip;
 sudo apt install -y zsh;
 sudo apt install -y geany;
@@ -22,9 +22,11 @@ sudo apt install -y ncdu;
 sudo apt install -y curl;
 sudo apt install -y gnome-tweak-tool;
 sudo apt install -y mono-complete;
-sudo apt install -y dpkg;
     
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --batch && echo "Install complete!"
+
+ZSH=/home/devin/.oh-my-zsh;
+ZSH_CUSTOM=/home/devin/.oh-my-zsh;
     
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
    
@@ -34,10 +36,54 @@ sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/
     
 sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
     
-sudo wget -o /usr/lib/keepass2/KeePassHttp.plgx https://raw.github.com/pfn/keepasshttp/master/KeePassHttp.plgx
+sudo wget -O /usr/lib/keepass2/KeePassHttp.plgx https://raw.github.com/pfn/keepasshttp/master/KeePassHttp.plgx
 
-sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="spaceship"/' .zshrc
-sudo sed -i 's/plugins=.*/plugins=(git, zsh-autosuggestions, zsh-syntax-highlighting)/g' .zshrc
+chsh -s $(which zsh);
 
-echo "Left to install: IntelliJ, dropbox, edit vim.rc" 
-echo "Todo: Edit vim rice file, side script with option for firefox addons"
+sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="spaceship"/' ~/.zshrc
+sudo sed -i 's/plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+
+clear
+echo "Installation Successful!"
+echo "Logout for ZSH to function properly"
+ 
+read -p "Would you like to install IntelliJ and Dropbox? [Y/n] " pack
+read -p "Would you like to install GTK Theme? [Y/n] " theme
+read -p "Would you like to install Firefox extensions? [Y/n] " fox
+
+if [[ "$pack" == 'Y' || "$pack" == 'y' ]] && [[ "$theme" == 'Y' || "$theme" == 'y' ]] && [[ "$fox" == 'Y' || "$fox" == 'y' ]]
+then
+    sudo ./themes.sh;
+    sudo ./packages.sh;
+    sudo ./ext.sh;
+    echo "Todo: Edit vim rice file, side script with option for firefox addons";
+elif [[ "$pack" == 'Y' || "$pack" == 'y' ]] && [[ "$theme" == 'Y' || "$theme" == 'y' ]] && [[ "$fox" == 'N' || "$fox" == 'n' ]]
+then
+    sudo ./themes.sh;
+    sudo ./packages.sh;
+    echo "Todo: Edit vim rice file, side script with option for firefox addons";
+elif [[ "$pack" == 'Y' || "$pack" == 'y' ]] && [[ "$theme" == 'N' || "$theme" == 'n' ]] && [[ "$fox" == 'N' || "$fox" == 'n' ]]
+then
+	sudo ./packages.sh
+	echo "Todo: Edit vim rice file, side script with option for firefox addons";
+elif [[ "$pack" == 'Y' || "$pack" == 'y' ]] && [[ "$theme" == 'N' || "$theme" == 'n' ]] && [[ "$fox" == 'Y' || "$fox" == 'y' ]]
+then
+	sudo ./packages.sh;
+	sudo ./ext.sh;
+	echo "Todo: Edit vim rice file, side script with option for firefox addons";
+elif [[ "$pack" == 'N' || "$pack" == 'n' ]] && [[ "$theme" == 'Y' || "$theme" == 'y' ]] && [[ "$fox" == 'Y' || "$fox" == 'y' ]]
+then
+	sudo ./themes.sh;
+	sudo ./ext.sh;
+	echo "Todo: Edit vim rice file, side script with option for firefox addons";
+elif [[ "$pack" == 'N' || "$pack" == 'n' ]] && [[ "$theme" == 'N' || "$theme" == 'n' ]] && [[ "$fox" == 'Y' || "$fox" == 'y' ]]
+then
+    sudo ./ext.sh;
+    echo "Todo: Edit vim rice file, side script with option for firefox addons";
+elif [[ "$pack" == 'N' || "$pack" == 'n' ]] && [[ "$theme" == 'Y' || "$theme" == 'y' ]] && [[ "$fox" == 'N' || "$fox" == 'n' ]]
+then
+    sudo ./themes.sh;
+    echo "Todo: Edit vim rice file, side script with option for firefox addons";
+else
+    echo "Todo: Edit vim rice file, side script with option for firefox addons";
+fi
