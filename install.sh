76b7692937f334cfa4bfb1edae545c006355f31c
Devin-Marks <dmarks2706@floridapoly.edu>
2019-03-27 09:28:42 -0400

    Removed keypass2 and added bitwarden

--- install.sh
+++ install.sh
@@ -8,20 +8,19 @@ sudo apt update;
 sudo snap install discord;
 sudo snap install telegram-desktop;
 sudo snap install spotify;
+sudo snap install bitwarden;
 sudo snap install vscode --classic;
 
 sudo apt install -y python3.6;
 sudo apt install -y python3-pip;
 sudo apt install -y zsh;
 sudo apt install -y geany;
-sudo apt install -y keepass2;
 sudo apt install -y vim;
 sudo apt install -y libreoffice;
 sudo apt install -y glances;
 sudo apt install -y ncdu;
 sudo apt install -y curl;
 sudo apt install -y gnome-tweak-tool;
-sudo apt install -y mono-complete;
     
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --batch && echo "Install complete!"
 
@@ -36,8 +35,6 @@ sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/
     
 sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
     
-sudo wget -O /usr/lib/keepass2/KeePassHttp.plgx https://raw.github.com/pfn/keepasshttp/master/KeePassHttp.plgx
-
 chsh -s $(which zsh);
 
 sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="spaceship"/' ~/.zshrc
@@ -51,9 +48,9 @@ clear
 echo "Installation Successful!"
 echo "Logout for ZSH to function properly"
  
-read -p "Would you like to install IntelliJ and Dropbox? [Y/n] " pack
+read -p "Would you like to install Intelli? [Y/n] " pack
 read -p "Would you like to install GTK Theme? [Y/n] " theme
-read -p "Would you like to install Firefox extensions? [Y/n] " fox
+read -p "Would you like to install Firefox profile? [Y/n] " fox
 
 if [[ "$pack" == 'Y' || "$pack" == 'y' ]] && [[ "$theme" == 'Y' || "$theme" == 'y' ]] && [[ "$fox" == 'Y' || "$fox" == 'y' ]]
 then
@@ -87,7 +84,7 @@ then
 elif [[ "$pack" == 'N' || "$pack" == 'n' ]] && [[ "$theme" == 'Y' || "$theme" == 'y' ]] && [[ "$fox" == 'N' || "$fox" == 'n' ]]
 then
     sudo ./themes.sh;
-    echo "Todo: Edit vim rice file, side script with option for firefox addons";
+    echo "Todo: Side Script with option for firefox addons";
 else
-    echo "Todo: Edit vim rice file, side script with option for firefox addons";
+    echo "Todo: Side script with option for firefox addons";
 fi
--- packages.sh
+++ packages.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt install -y dpkg
-
-wget -O ./dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2019.02.14_amd64.deb
-
-sudo dpkg -i dropbox.deb
-dropbox start -i
-
 wget -O ./IDEA.tar.gz https://download.jetbrains.com/idea/ideaIC-2018.3.5.tar.gz
 
 tar -xvzf IDEA.tar.gz
 mv idea-IC-183.5912.21 ~/IDEA
 sudo ~/IDEA/bin/idea.sh
 
-rm dropbox.deb
 rm IDEA.tar.gz
 
 exit 1;
